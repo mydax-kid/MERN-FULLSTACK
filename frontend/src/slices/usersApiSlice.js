527daf0e2d1f76dfc5a0bf64c6d16e12149e25af
samuel <samuelwachira007@gmail.com>
2023-09-21 21:46:03 +0300

    first commit

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,54 @@
+import { apiSlice } from './apiSlice';
+const USERS_URL = '/api/users';
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST',
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`, 
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useUpdateUserMutation,
+  // useGetDataQuery,
+} = userApiSlice;
+
+
+//queries are used for fetching data while 
+//mutations are used when sending or changing data in the backend
+// getData: builder.query({
+    //   query: (data) => ({
+    //     url: `${USERS_URL}/profile`,
+    //     method: 'GET',
+    //   }),
+    // }),
